96929b815097bdf5045b4ae7e72122c26e7ccc5e
Tanner Chen <chen.tanner@gmail.com>
2023-06-11 13:24:10 -0600

    Added publishing and css support

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
-
+import './Button.css';
 
 interface ButtonProps {
   label: string;
 }
- 
+
 const Button: FC<ButtonProps> = (props: ButtonProps) => {
   return (
     <button>{props.label}</button>
-    );
+  );
 }
- 
+
 export default Button;
